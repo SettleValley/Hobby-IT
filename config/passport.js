42b6887389fa6716e3beb7d2c0539bf0a1303033
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2018-09-22 23:10:39 -0500

    rewrite code/ECMA6, succesfull signup, DONE

--- config/passport.js
+++ config/passport.js
@@ -1,53 +1,49 @@
 'use strict'
-var passport = require('passport');
-var User = require('../models/user');
-var LocalStrategy  = require('passport-local').Strategy;
-
-passport.serializeUser(function(user, done){
-    done(null, user.id);
-});
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        done(err, user);
-    });
-});
-
-
+const passport = require('passport');
+const User = require('../models/user');
+const LocalStrategy  = require('passport-local').Strategy;
+// Passport serializer controller callback function be called
+passport.serializeUser((user, done)=>{
+  done(null, user.id)
+})
+passport.deserializeUser((id, done)=>{
+  User.findById(id, (err, user)=>{
+    done(err, user)
+  })
+})
 // User Sign up strategy
 passport.use('local.signup', new LocalStrategy ({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-}, function(req, email, password, done){
-    req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
-    req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
-    var errors = req.validationErrors();
-    if(errors){
-        var messages = [];
-        errors.forEach(function(error){
-            messages.push(error.msg);
-        });
-        return done(null, false, req.flash('error', messages));
+}, (req, email, password, done)=>{
+  req.checkBody('email', 'Invalid Email').notEmpty().isEmail();
+  req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+  let errors = req.validationErrors();
+  if (errors) {
+    const messages = []
+    errors.forEach( (error)=>{
+          messages.push(error.msg)
+    })
+    return done(null, false, req.flash('error', messages))
+  }
+  User.findOne({'email': email},(err, user)=>{
+    if (err) {
+        return done(err)
     }
-    User.findOne({'email': email}, function(err, user){
-        if (err) {
-            return done(err);
-        }
-        if (user){
-            return done(null, false, {message: 'El correo ya esta utilizado'});
-        }
+    if (user){
+        return done(null, false, {message: 'El correo ya esta utilizado'})
+    }
+    let newUser = new User()
+    newUser.email = email
+    newUser.password = newUser.encryptPassword(password)
+    newUser.save((err, result)=>{
+      if(err){
+          return done(err);
+      }
+      console.log("paso bien");
 
-        var newUser = new User();
-        newUser.email = email;
-        newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err, result){
-            if(err){
-                return done(err);
-            }
-            console.log("paso bien");
-
-            return done(null, newUser);
-        });
-
-    });
+      return done(null, newUser);
+    })
+  })
 }));
