3c8755b2c66d171806b11d1eb29cc52997a3368e
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2018-10-15 01:39:19 -0500

    agregar valores de longitud y latitud en el mapa al formulario #13

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,7 +3,6 @@ $(function(){
     var lat = $('#map').attr('lat') || -34.397;
     var lng = $('#map').attr('lng') || 150.644;
     var title = $('#map').attr('title');
-console.log(lat);
   initMap(12, lat, lng, "", "");
   function initMap(valZoom, lat, lon, valTitle, info) {
     geocoder =  new google.maps.Geocoder();
@@ -11,7 +10,7 @@ console.log(lat);
       // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
       var mapOptions = {
           // How zoomed in you want the map to start at (always required)
-          zoom: valZoom,
+          zoom: 12,
 
           // The latitude and longitude to center the map (always required)
           center: new google.maps.LatLng(lat, lon),
@@ -218,9 +217,9 @@ console.log(lat);
                       lat: position.coords.latitude,
                       lng: position.coords.longitude
                   };
-
                   infoContent.setPosition(pos);
-                  infoContent.setContent('Location found.');
+
+                  infoContent.setContent('your right now');
                   infoContent.open(map);
                   map.setCenter(pos);
               }, function () {
@@ -229,6 +228,91 @@ console.log(lat);
           }else{
               handleLocationError(false, infoContent, map.getCenter());
           }
+
+            //Buscador
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        //this is to add the view like google map have to example but we dont want to modify us style
+        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function () {
+            searchBox.setBounds(map.getBounds());
+        });
+        //pin click event
+        map.addListener('click', function(e){
+            placeMarkerAndPanTo(e.latLng);
+        });
+      function placeMarkerAndPanTo(location) {
+          if(!marker || !marker.setPosition){
+              marker = new google.maps.Marker({
+                  position: location,
+                  map: map,
+              });
+          }else{
+              marker.setPosition(location);
+          }
+          if (!!infowindow && !!infowindow.close) {
+              infowindow.close();
+          }
+
+          infowindow = new google.maps.InfoWindow({
+              content: 'Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
+          });
+          addInputLocation(location.lat(), location.lng());
+          infowindow.open(map, marker);
+      }
+      google.maps.event.addDomListener(window, 'load', initMap);
+      function addInputLocation(lat, lng){
+          $('input[name=lat]').val(lat);
+          $('input[name=lng]').val(lng);
+      }
+      //end pin
+      var MarkerFound = [];
+      // Listen for the event fired when the user selects a prediction and retrieve
+      // more details for that place.
+      searchBox.addListener('places_changed', function(){
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+              return;
+          }
+          // Clear out the old markers.
+          MarkerFound.forEach(function (marker) {
+              marker.setMap(null);
+          });
+          MarkerFound = [];
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function (place){
+              if (!place.geometry) {
+                  console.log("Returned place contains no geometry");
+                  return;
+              }
+              var icon = {
+                  url: place.icon,
+                  size: new google.maps.Size(71, 71),
+                  origin: new google.maps.Point(0, 0),
+                  anchor: new google.maps.Point(17, 34),
+                  scaledSize: new google.maps.Size(25, 25)
+              };
+              // Create a marker for each place.
+              MarkerFound.push(new google.maps.Marker({
+                  map: map,
+                  icon: icon,
+                  title: place.name,
+                  position: place.geometry.location
+              }));
+              if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+              } else {
+                  bounds.extend(place.geometry.location);
+              }
+          });
+          map.fitBounds(bounds);
+      });
+
+        
 
 
   }
@@ -239,6 +323,7 @@ console.log(lat);
             'Error: Your browser doesn\'t support geolocation.');
         infoContent.open(map);
     }
+
 //end init map
 
 });
